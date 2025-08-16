541d03d73a9be429170808b333a40d2a9900c91b
Ashishk11111 <ashishkashyap72495@gmail.com>
2025-08-16 21:03:55 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -199,5 +199,5 @@ socket.on('join-room', async ({ roomId }) => {
 });
 
 // ---------------- Start server ----------------
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
